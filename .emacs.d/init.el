02a8a351526c2af5aadb64acb7d759ff8d0ab218
Ian Mahuron <ian@mahuron.org>
2012-01-19 13:27:54 -0800

    early commit

---
+++ .emacs.d/init.el
@@ -0,0 +1,43 @@
+;; set load-path
+(let ((default-directory "~/.emacs.d/lisp/"))
+  (normal-top-level-add-to-load-path '("."))
+  (normal-top-level-add-subdirs-to-load-path))
+
+;; start emacs server
+(server-start)
+
+;; general ui options
+(setq inhibit-splash-screen t)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(menu-bar-mode -1)
+
+;; selection options
+(setq transient-mark-mode nil)
+(setq mouse-yank-at-point t)
+
+;; disable warnings for "advanced" functions
+(put 'narrow-to-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+
+;; built in version control
+(setq
+ backup-by-copying t
+ backup-directory-alist '(("." . "~/.saves"))
+ delete-old-versions t
+ kept-new-versions 6
+ kept-old-versions 2
+ version-control t)
+
+;; themes
+(require 'color-theme)
+(color-theme-initialize)
+(color-theme-arjen)
+
+;; shorten literal lambda to greek character
+(require 'pretty-lambdada)
+(pretty-lambda-for-modes)
+
+;; python
+(add-hook 'python-mode-hook 'turn-on-pretty-lambda-mode)
--- .zshrc
+++ .zshrc
@@ -10,6 +10,12 @@ setopt extended_glob
 # LS_COLORS
 eval $(dircolors -b ~/.dircolors)
 
+case ${TERM} in
+	(*)
+		export PS1="%B[%b%3>>%n%>>@%5>>%m%>>%B]%b %2~ %! %B%#%b "
+	;;
+esac
+
 # init completion
 autoload -U compinit
 compinit -i
