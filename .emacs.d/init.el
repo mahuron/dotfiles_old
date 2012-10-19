168a70612249f55a9a04be978c9fa5c5afa730ee
Ian Mahuron <ian@mahuron.org>
2012-10-19 10:12:43 -0700

    Primarily changes to emacs c-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,8 @@
 ;; set load-path
-(let ((default-directory "~/.emacs.d/lisp/"))
-  (normal-top-level-add-to-load-path '("."))
-  (normal-top-level-add-subdirs-to-load-path))
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+(add-to-list 'load-path "~/.emacs.d/lisp/magit")
+(add-to-list 'load-path "~/.emacs.d/lisp/color-theme")
+(add-to-list 'load-path "~/.emacs.d/lisp/python-mode")
 
 ;; start emacs server
 (server-start)
@@ -42,16 +43,51 @@
 
 ;; shorten literal lambda to greek character
 (require 'pretty-lambdada)
+(add-to-list 'pretty-lambda-auto-modes 'python-mode)
 (pretty-lambda-for-modes)
 
 ;; magit
 (require 'magit)
 
+;; whitespace mode
+(setq whitespace-style '(face tabs trailing lines space-before-tab space-after-tab empty))
+
 ;; python
-(add-hook 'python-mode-hook 'turn-on-pretty-lambda-mode)
+(require 'python-mode)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
+(setq py-install-directory "~/.emacs.d/lisp/python-mode")
+(setq py-shell-name "ipython")
+
+;; lisp
+(add-hook 'emacs-lisp-mode-hook '(lambda () (setq indent-tabs-mode 'nil)))
 
 ;; c
-(setq 
- c-default-style "k&r"
- c-basic-offset 4)
-(add-hook 'c-mode-common-hook '(lambda () (c-toggle-auto-state 1)))
+(defconst f5-c-style
+  '((c-basic-offset             .  4)
+    (c-comment-only-line-offset .  (0 . 0))
+    (c-offsets-alist            .  ((statement-block-intro . +)
+                                    (knr-argdecl-intro     . 5)
+                                    (substatement-open     . 0)
+                                    (label                 . 0)
+                                    (statement-case-open   . +)
+                                    (statement-cont        . +)
+                                    (arglist-intro . c-lineup-arglist-intro-after-paren)
+                                    (arglist-close . c-lineup-arglist)
+                                    (inline-open           . 0)
+                                    ))
+    (c-special-indent-hook       .  c-gnu-impose-minimum)
+    (c-comment-continuation-stars . "")
+    (c-hanging-comment-ender-p    . t)
+   )
+  "f5-c-style")
+
+(defun f5-c-mode-common-hook ()
+  (c-add-style "f5-c-style" f5-c-style t)
+  (setq indent-tabs-mode 'nil)
+  (setq tab-width 4)
+  (c-toggle-auto-hungry-state 1)
+  (whitespace-mode)
+)
+
+(add-hook 'c-mode-common-hook 'f5-c-mode-common-hook)
